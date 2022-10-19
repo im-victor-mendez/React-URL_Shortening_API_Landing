a18c14e85ffeb2f3d549148a607868818061789f
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-10-18 20:24:20 -0500

    Created Top layout.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/layout/Header';
+import Top from './components/layout/Top';
 
 function App() {
   return <div className="App">
     <Header />
+    <Top />
     </div>
 }
 
---
+++ src/components/layout/Top.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../common/Button'
+
+function Top() {
+  return <section id='top'>
+    <div id='text'>
+        <h1>More than just shorter links</h1>
+        <p>Build yout brand's recognition and get datailed insights on how your links are performing.</p>
+        <Button id={'get-started'} text={'Get Started'} />
+    </div>
+
+    <img src="" alt="" />
+  </section>
+}
+
+export default Top
