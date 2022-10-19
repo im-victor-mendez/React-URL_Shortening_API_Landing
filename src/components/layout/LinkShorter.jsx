01e6a753af58ece60931d70789de529bc66a5d9a
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-10-19 12:22:06 -0500

    Implemented apiShortening() in LinkShorter layout.

--- src/components/layout/LinkShorter.jsx
+++ src/components/layout/LinkShorter.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { apiShortening } from '../../utils/api'
 import Button from '../common/Button'
 
 function LinkShorter() {
-    const [link, setLink] = useState()
+    const [link, setLink] = useState({original: '', shorter: ''})
     const [list, setList] = useState([])
 
     useEffect(() => {
@@ -11,7 +12,6 @@ function LinkShorter() {
         if(localList) setList(JSON.parse(localList))
     }, [])
     
-
     useEffect(() => {
       localStorage.setItem('List', JSON.stringify(list))
     }, [list])
@@ -20,28 +20,62 @@ function LinkShorter() {
     function handleLink(event) {
         event.preventDefault()
         
+        //console.log(link)
+        if(!link.shorter) return
         setList(
             [...list, link]
         )
-        setLink('')
+        
+        setLink({original: ''})
+    }
+
+
+
+    function shortURL(event) {
+        apiShortening(link.original)
+        .then(
+            data => {
+                const dataset = data.result
+                //console.log(link.original, dataset, dataset.original_link, dataset.short_link)
+                setLink(
+                    {
+                        original: link.original,
+                        shorter: dataset.short_link
+                    }
+                )
+                //console.log(link)
+            }
+        )
+
+        handleLink(event)
     }
 
     return <section id='link-shorter'>
-        <form id='form' onSubmit={handleLink}>
+        <form id='form' onSubmit={shortURL}>
             <input
-            type="text"
+            type={'url'}
+            required
             name="link"
             id="link"
             placeholder='Shorten a link here...'
-            value={link}
-            onChange={event => setLink(event.target.value)} />
+            value={link.original}
+            onChange={
+                event => setLink(
+                    {
+                        original: event.target.value
+                    }
+                )
+            } />
             <Button id={'shorter'} text={'Shorten It!'} />
         </form>
 
         <div id='links'>
             {
                 list.map(
-                    link => link
+                    link => <div>
+                        {link.original}
+                        {link.shorter}
+                    </div>
                 )
             }
         </div>
